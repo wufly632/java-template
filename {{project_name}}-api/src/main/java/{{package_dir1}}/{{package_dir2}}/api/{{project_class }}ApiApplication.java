a36f5e0b4001d522ef1841aeda4ea2fac2cb2210
flynn <fei.wu@coraool.com>
2025-02-24 23:04:24 +0800

    update

--- {{project_name}}-api/src/main/java/{{package_dir1}}/{{package_dir2}}/api/{{project_class }}ApiApplication.java
+++ {{project_name}}-api/src/main/java/{{package_dir1}}/{{package_dir2}}/api/{{project_class }}ApiApplication.java
@@ -1,9 +1,9 @@
 package {{package_name}}.api;
 
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
-import {{package_name}}.core.{{ cookiecutter.project_class }}CoreApplication;
-import {{package_name}}.integration.{{ cookiecutter.project_class }}IntegrationConfiguration;
-import {{package_name}}.repository.{{ cookiecutter.project_class }}RepositoryConfiguration;
+import {{package_name}}.core.{{ project_class }}CoreApplication;
+import {{package_name}}.integration.{{ project_class }}IntegrationConfiguration;
+import {{package_name}}.repository.{{ project_class }}RepositoryConfiguration;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,19 +18,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ServletComponentScan
 @Import(value = { 
-    {{ cookiecutter.project_class }}RepositoryConfiguration.class,
-    {{ cookiecutter.project_class }}IntegrationConfiguration.class,
-    {{ cookiecutter.project_class }}CoreApplication.class 
+    {{ project_class }}RepositoryConfiguration.class,
+    {{ project_class }}IntegrationConfiguration.class,
+    {{ project_class }}CoreApplication.class 
 })
 @PropertySource("classpath:env-application.properties")
 @ComponentScan(basePackages = "com.coraool")
 @EnableDubbo(scanBasePackages = {"{{package_name}}.core"})
 @EnableScheduling
-@NacosPropertySource(dataId = "nacos.coraool.{{ cookiecutter.project_name.lower().replace('-', '.') }}", groupId = "nacos.config", autoRefreshed = true)
-public class {{ cookiecutter.project_class }}ApiApplication {
+@NacosPropertySource(dataId = "nacos.coraool.{{nacos_data_id}}", groupId = "nacos.config", autoRefreshed = true)
+public class {{ project_class }}ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run({{ cookiecutter.project_class }}ApiApplication.class, args);
+        SpringApplication.run({{ project_class }}ApiApplication.class, args);
     }
 
 }
