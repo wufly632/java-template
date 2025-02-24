6d3dee4bd3de44028ae4621aa254af83f6ccee25
flynn <fei.wu@coraool.com>
2025-02-24 17:43:21 +0800

    update

--- {{cookiecutter.project_name}}-commons/src/main/java/{{cookiecutter.package_dir}}/commons/{{ cookiecutter.project_class }}CommonsConfiguration.java
+++
@@ -1,7 +0,0 @@
-package {{cookiecutter.package_name}}.commons;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class {{cookiecutter.project_class}}CommonsConfiguration {
-}
---
+++ {{project_name}}-commons/src/main/java/{{cookiecutter.package_dir}}/commons/{{project_class }}CommonsConfiguration.java
@@ -0,0 +1,7 @@
+package {{package_name}}.commons;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class {{project_class}}CommonsConfiguration {
+}
