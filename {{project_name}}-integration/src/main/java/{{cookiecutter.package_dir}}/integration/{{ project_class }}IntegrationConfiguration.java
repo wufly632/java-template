921ccec67f73fee9f59c4b30def3099c16cddf63
flynn <fei.wu@coraool.com>
2025-02-24 17:43:21 +0800

    update

---
+++ {{project_name}}-integration/src/main/java/{{cookiecutter.package_dir}}/integration/{{ project_class }}IntegrationConfiguration.java
@@ -0,0 +1,11 @@
+package {{package_name}}.integration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@ComponentScan
+@Configuration
+@PropertySource("classpath:{cookiecutter.slug}-integration.properties")
+public class {{ cookiecutter.project_class }}IntegrationConfiguration {
+}
